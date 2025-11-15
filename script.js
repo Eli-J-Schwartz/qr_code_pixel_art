bd958750337be93575f4091c7ae9d494677dccf1
Eli Schwartz <eli.jonathan.schwartz@gmail.com>
2025-11-15 14:53:13 -0500

    Implemented ecc

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ function generate() {
     drawPixels(createConstantPatterns(), 41);
 
     formatText(document.getElementById("inputText").value);
+
+    console.log(generateCodewords(formatText(document.getElementById("inputText").value)))
 }
 
 function createConstantPatterns() {
-    qrArr = new Array(41*41).fill(1);
+    var qrArr = new Array(41*41).fill(1);
 
     for (var i = 0; i < 41; i++) {
         qrArr[6+41*i] = (i%2 == 0) ? 0 : 1;
@@ -67,11 +69,11 @@ function createConstantPatterns() {
 }
 
 function formatText(text) {
-    bytes = new TextEncoder().encode(text);
-    len = text.length;
+    var bytes = new TextEncoder().encode(text);
+    var len = text.length;
     console.log(bytes);
 
-    output = new Array(60);
+    var output = new Array(60);
     output[0] = 64 + (len>>4);
     output[1] = ((len&15) << 4) + (bytes[0] >> 4);
     for (var i = 1; i < len; i++) {
@@ -79,21 +81,71 @@ function formatText(text) {
     }
     output[len+1] = ((bytes[len-1]&15) << 4);
 
-    padbyte = 0xec;
+    var padbyte = 0xec;
     for (var i = len+2; i < 60; i++) {
         output[i] = padbyte;
         padbyte ^= (0xec ^ 0x11);
     }
 
     console.log(output);
+
+    return output;
+}
+
+function GFMult(a, b) {
+    var out = 0;
+
+    for (var i = 0; i < 8; i++) {
+        if ((a >> i) & 1 == 1) {
+            out ^= b;
+        }
+        b = ((b<<1) & 0xff) ^ ((b>>7) * 0x1D);
+    }
+
+    return out;
+}
+
+function polyDiv(a, b) {
+    //console.log(a.slice());
+    while (a.length > 28) {
+        for (var i = 1; i < 29; i++) {
+            a[i] ^= GFMult(b[i], a[0])
+        }
+        a.shift();
+    }
+    //console.log(a.slice());
+    return a;
+}
+
+function generateECC(data) {
+    var generatorPoly = [1, 252, 9, 28, 13, 18, 251, 208, 150, 103, 174, 100, 41, 167, 12, 247, 56, 117, 119, 233, 127, 181, 100, 121, 147, 176, 74, 58, 197];
+
+    return polyDiv(data.concat(new Array(28).fill(0)).slice(), generatorPoly);
+}
+
+function generateCodewords(data) {
+    var out = new Array(43*4).fill(0);
+
+    //console.log(out);
+    for (var i = 0; i < 4; i++) {
+        ecc = generateECC(data.slice(15*i, 15*(i+1)));
+        for (var j = 0; j < 15; j++) {
+            out[i+4*j] = data[i*15+j];
+        }
+        for (var j = 15; j < 43; j++) {
+            out[i+4*j] = ecc[j-15];
+        }
+    }
+
+    return out;
 }
 
 function drawPixels(pixels, width) {
-    height = Math.ceil(pixels.length / width);
+    var height = Math.ceil(pixels.length / width);
 
-    canvas = document.createElement('canvas');
-    context = canvas.getContext('2d');
-    imgData = context.createImageData(width, height);
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    var imgData = context.createImageData(width, height);
 
     canvas.height = height;
     canvas.width = width;
